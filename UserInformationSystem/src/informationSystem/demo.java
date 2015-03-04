e235d9d21bde7344597ed1b198ca62bb143e3117
786sag <shahid.ahmed@zigron.com>
2015-03-04 09:16:44 +0500

    Updated version for collection assignment

--- UserInformationSystem/src/informationSystem/demo.java
+++ UserInformationSystem/src/informationSystem/demo.java
@@ -8,61 +8,35 @@ package informationSystem;
 public class demo {
 
 	public static void main(String[] args) {
-		User shahid=new User();
-		User father = new User();
-		User mother = new User();
+		final User shahid=new User();
+		User father = new User();		
 		User ali=new User();
+		User ahmad=new User();
 		
 		//setting shahid's properties
-			shahid.setfName("Shahid");
-			shahid.setmName("Ahmad");
-			shahid.setlName("Ghumman");
-			shahid.setAddress("Sialkot,51301,street no 9,Pakistan");
-			shahid.setAge(30);
-			shahid.setPhone("+ 92-3218798798");
-			shahid.setGender("Male");
-			//setting Father's properties
-			father.setfName("Noor");
-			father.setmName("Ahmad");
-			father.setlName("Ghumman");
-			father.setAddress("Sialkot,51301,street no 9,Pakistan");
-			father.setAge(60);
-			father.setPhone("+923275847584");
-			father.setGender("Male");
-			//setting Mother's properties
-			mother.setfName("Ayesha");
-			mother.setmName("Noor");
-			mother.setlName("Ahmad");
-			mother.setAddress("Sialkot,51301,street no 9,Pakistan");
-			mother.setAge(55);
-			mother.setPhone("+923275847667");
-			mother.setGender("Female");
-			//Putting key and value in the map for father
-			father.getRelatives().put("son", shahid);			
-			shahid.getRelatives().put("father", father);			
-			father = shahid.getRelatives().get("father");
-			father.getRelatives().get("son");
+			shahid.setUserInformation("shahid", "ahmad", "ghumman", "Sialkot,51301,street no 9,Pakistan", "+ 92-3218798798", 1, 30,"Male" );
 			
-			//Putting key and value in the map for Ali(second son)
-			father.getRelatives().put("son", ali);
-			ali.getRelatives().put("father", father);
-			father=shahid.getRelatives().get("father");
-			father.getRelatives().get("son");
+		//setting Father's properties
+			father.setUserInformation("noor", "ahmad", "ghumman", "Sialkot,51301,street no 9,Pakistan", "+ 92-3218798333", 2, 50,"Male" );
 			
+		//setting shahid's properties
+			shahid.setUserInformation("ahmad", "shah", "ghumman", "Sialkot,51301,street no 9,Pakistan", "+ 92-3218722223", 1, 9,"Male" );
+		//Methid to find father name with shahid object
+			shahid.getFatherName(father, shahid);
 			
-			//Putting key and value in the map for mother
-		    mother.getRelatives().put("son", shahid);
-		    shahid.getRelatives().put("Mother", mother);
-		    mother=shahid.getRelatives().get("mother");
-		    //mother.getRelatives().get("son");
+		//Method to find father name with ali object			
+			ali.getFatherName(father, ali);			
+		    
+		//grandfather query
+		    ali.getRelatives().put("son", ahmad);
+		    ahmad.getRelatives().put("father", ali);
+		  
+		//System.out.println("ahmed grand father === "+ahmad.getRelatives().get("father").getRelatives().get("father").getfName());
+		    
 			
 	
-			//Getting the First son's father's first name and mother's first name
-		   System.out.println("First son's name is "+shahid.getfName());
-		   System.out.println("Shahid's father's first name is "+shahid.getRelatives().get("father").getfName());
-		   System.out.println("Second sons's name is "+ali.getfName());
-		   System.out.println("Ali's father's first name is "+shahid.getRelatives().get("father").getfName());
-		   System.out.println("Father's second son's name is  "+father.getRelatives().get("son").getfName());
+		    System.out.println("Shahid's father's first name is "+shahid.getRelatives().get("father").getfName());
+		    System.out.println("Ali's father's first name is "+shahid.getRelatives().get("father").getfName());
 	}
 	
 
